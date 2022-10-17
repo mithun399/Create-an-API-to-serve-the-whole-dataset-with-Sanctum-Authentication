c1a50b85f4f8d601f138ef30a02d7a9cc1613b79
Mithun Sutradhar <msovik399@gmail.com>
2022-10-17 23:31:32 +0600

    serve the whole dataset with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\apiController;
+use App\Http\Controllers\userController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +17,14 @@ use App\Http\Controllers\apiController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 Route::get("add",[apiController::class,"addData"]);
-Route::get("get",[apiController::class,"getData"]);
+Route::post("login",[userController::class,'index']);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    
+    Route::get("get",[apiController::class,"getData"]);
+
+    });
